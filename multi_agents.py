4e52eaf7a708e513429d252c35eead7707106855
Yair Finesilver <yairfine@apple.com>
2022-04-13 11:15:53 +0300

    alpha-beta one method

--- multi_agents.py
+++ multi_agents.py
@@ -149,7 +149,8 @@ class MinmaxAgent(MultiAgentSearchAgent):
             Returns the successor game state after an agent takes an action
         """
         legal_moves = game_state.get_agent_legal_actions()
-        action_to_score = {action: self.minimax(game_state, self.depth, player=0) for action in legal_moves}
+        action_to_score = {action: self.minimax(game_state.generate_successor(agent_index=0, action=action), self.depth, player=0)
+                           for action in legal_moves}
         best_action = max(action_to_score, key=action_to_score.get)
         return best_action
 
@@ -206,45 +207,83 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         """
         Returns the minimax action using self.depth and self.evaluationFunction
         """
+        legal_moves = game_state.get_agent_legal_actions()
+        action_to_score = {action: self.alpha_beta(game_state.generate_successor(agent_index=0, action=action), self.depth, -np.inf, np.inf, player=0)
+                           for action in legal_moves}
+        best_action = max(action_to_score, key=action_to_score.get)
+        return best_action
 
-        legal_moves = game_state.get_legal_actions(agent_index=0)
-        scores = [self.min_value(game_state.generate_successor(agent_index=0, action=action), self.depth - 1, -np.inf, np.inf) for
-                  action in legal_moves]
-        best_score = max(scores)
-        best_indices = [index for index in range(len(scores)) if scores[index] == best_score]
-        chosen_index = np.random.choice(best_indices)  # Pick randomly among the best
-
-        return legal_moves[chosen_index]
-
-    def max_value(self, state: GameState, depth: int, alpha: float, beta: float) -> float:
+    def alpha_beta(self, state: GameState, depth: int, alpha: float, beta: float, player: int) -> float:
         if state.done or depth == 0:
             return self.evaluation_function(state)
 
-        val = -np.inf
+        if player == 0:
+            val = -np.inf
 
-        for action in state.get_legal_actions(agent_index=0):
-            val = max(val, self.min_value(state.generate_successor(agent_index=0, action=action),
-                                          depth - 1, alpha, beta))
-            if val >= beta:
-                break  # beta cutoff
-            alpha = max(alpha, val)
+            for action in state.get_legal_actions(agent_index=player):
+                val = max(val, self.alpha_beta(state.generate_successor(agent_index=player, action=action),
+                                               depth - 1, alpha, beta, player=1 - player))
+                if val >= beta:
+                    break  # beta cutoff
+                alpha = max(alpha, val)
 
-        return val
+            return val
 
-    def min_value(self, state: GameState, depth: int, alpha: float, beta: float) -> float:
-        if state.done or depth == 0:
-            return self.evaluation_function(state)
+        else:  # player == 1
+            val = np.inf
 
-        val = np.inf
+            for action in state.get_legal_actions(agent_index=player):
+                val = min(val, self.alpha_beta(state.generate_successor(agent_index=player, action=action),
+                                               depth, alpha, beta, player=1 - player))
+                if val <= alpha:
+                    break  # alpha cutoff
+                beta = min(beta, val)
 
-        for action in state.get_legal_actions(agent_index=1):
-            val = min(val, self.max_value(state.generate_successor(agent_index=1, action=action),
-                                          depth, alpha, beta))
-            if val <= alpha:
-                break  # alpha cutoff
-            beta = min(beta, val)
+            return val
 
-        return val
+    # def get_action(self, game_state):
+    #     """
+    #     Returns the minimax action using self.depth and self.evaluationFunction
+    #     """
+    #
+    #     legal_moves = game_state.get_legal_actions(agent_index=0)
+    #     scores = [self.min_value(game_state.generate_successor(agent_index=0, action=action), self.depth - 1, -np.inf, np.inf) for
+    #               action in legal_moves]
+    #     best_score = max(scores)
+    #     best_indices = [index for index in range(len(scores)) if scores[index] == best_score]
+    #     chosen_index = np.random.choice(best_indices)  # Pick randomly among the best
+    #
+    #     return legal_moves[chosen_index]
+    #
+    # def max_value(self, state: GameState, depth: int, alpha: float, beta: float) -> float:
+    #     if state.done or depth == 0:
+    #         return self.evaluation_function(state)
+    #
+    #     val = -np.inf
+    #
+    #     for action in state.get_legal_actions(agent_index=0):
+    #         val = max(val, self.min_value(state.generate_successor(agent_index=0, action=action),
+    #                                       depth - 1, alpha, beta))
+    #         if val >= beta:
+    #             break  # beta cutoff
+    #         alpha = max(alpha, val)
+    #
+    #     return val
+    #
+    # def min_value(self, state: GameState, depth: int, alpha: float, beta: float) -> float:
+    #     if state.done or depth == 0:
+    #         return self.evaluation_function(state)
+    #
+    #     val = np.inf
+    #
+    #     for action in state.get_legal_actions(agent_index=1):
+    #         val = min(val, self.max_value(state.generate_successor(agent_index=1, action=action),
+    #                                       depth, alpha, beta))
+    #         if val <= alpha:
+    #             break  # alpha cutoff
+    #         beta = min(beta, val)
+    #
+    #     return val
 
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
